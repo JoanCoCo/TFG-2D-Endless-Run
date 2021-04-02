ea11eb2ba025ea78802e4b6c42cb01b1e6d080a3
Joan Colom Colom <joacoco@inf.upv.es>
2021-04-02 18:20:29 +0200

    Improved course generation.

--- Assets/Scripts/CourseGenerator.cs
+++ Assets/Scripts/CourseGenerator.cs
@@ -42,14 +42,13 @@ public class CourseGenerator : MonoBehaviour
 
     private void GenerateNewCol()
     {
-        int currentHeight = _rows;
+        bool topFound = false;
         for (int i = _rows - 1; i >= 0; i--)
         {
             Transform spawnPoint = _origin;
             spawnPoint.position = new Vector3(_currentCol * cellSizeX + _initialX,
                 i * cellSizeY + _initialY, 0);
             GameObject o = Instantiate(suggestCell(i));
-            if (!o.GetComponent<Cell>().IsConnected(Cell.DOWN)) currentHeight--;
             o.transform.position = spawnPoint.position;
             o.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(
                 RectTransform.Axis.Vertical, cellSizeY);
@@ -64,10 +63,12 @@ public class CourseGenerator : MonoBehaviour
                 }
             }
             _newCol[i] = o;
+            if (o.GetComponent<Cell>().IsConnected(Cell.RIGHT) && !topFound)
+            {
+                maxHeight = Mathf.Min(i + 2, _rows);
+                topFound = true;
+            }
         }
-
-        Debug.Log("Current height = " + currentHeight.ToString());
-        maxHeight = Random.Range(Mathf.Max(2, currentHeight - 2), Mathf.Min(_rows, currentHeight + 2) + 1);
         Debug.Log("Next max height = " + maxHeight.ToString());
 
         for (int i = 0; i < _rows; i++)
@@ -81,46 +82,29 @@ public class CourseGenerator : MonoBehaviour
     {
         bool found = false;
         GameObject candidate = null;
-        int counterM = 0;
-        while(!found)
+        int countI = 0;
+        bool covered = (row < _rows - 1) ? _newCol[row + 1].GetComponent<Cell>().IsConnected(Cell.DOWN) : false;
+        while (!found)
         {
             candidate = _cells[Random.Range(0, _cells.Length)];
             Cell c = candidate.GetComponent<Cell>();
-            if(_previousCol[row] == null)
+            if (row > maxHeight - 1)
             {
-                if(row == 0)
-                {
-                    found = c.IsConnected(Cell.UP) & c.IsConnected(Cell.DOWN) &
-                        c.IsConnected(Cell.RIGHT) & c.IsConnected(Cell.LEFT);
-                } else
-                {
-                    found = !c.IsConnected(Cell.DOWN);
-                }
-            } else
+                found = !c.IsConnected(Cell.DOWN);
+            } else if(row >= maxHeight - 2 && row > 0 && !covered)
             {
                 Cell p = _previousCol[row].GetComponent<Cell>();
-                if(row >= maxHeight - 1)
-                {
-                    found = !c.IsConnected(Cell.DOWN);
-                } else if(row > 0)
-                {
-                    Cell b = _newCol[row + 1].GetComponent<Cell>();
-                    found = !b.IsConnected(Cell.DOWN) || c.IsConnected(Cell.UP);
-                    found &= c.IsConnected(Cell.UP) || !c.IsConnected(Cell.LEFT) || p.IsConnected(Cell.RIGHT);
-                    found &= c.IsConnected(Cell.UP) || !p.IsConnected(Cell.RIGHT) || c.IsConnected(Cell.LEFT);
-                } else
-                {
-                    //Cell b = _newCol[row + 1].GetComponent<Cell>();
-                    found = c.IsConnected(Cell.UP) && c.IsConnected(Cell.DOWN) &&
-                        c.IsConnected(Cell.RIGHT) && c.IsConnected(Cell.LEFT);
-                    //found |= !c.IsConnected(Cell.DOWN) && !b.IsConnected(Cell.DOWN);
-                }
-            }
-            counterM++;
-            if(counterM > 2000)
+                found = !c.IsConnected(Cell.LEFT) || p.IsConnected(Cell.RIGHT);
+                found &= !p.IsConnected(Cell.RIGHT) || c.IsConnected(Cell.LEFT);
+            } else
             {
-                found = true;
+                found = c.IsConnected(Cell.UP) & c.IsConnected(Cell.DOWN) & c.IsConnected(Cell.LEFT) & c.IsConnected(Cell.RIGHT);
+            }
+            countI++;
+            if(countI > 2000)
+            {
                 _red = true;
+                found = true;
             }
         }
         return candidate;
